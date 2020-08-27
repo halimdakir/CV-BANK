1ca873536a583d3b1d1e0902e3d03a1c9fb1d661
Puttzon <puttzon@gmail.com>
2020-08-27 20:10:15 +0200

    Installerat Bootstrap samt skapat header och login sida

---
+++ Frontend/cv-bank/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header id="header">
+      <a href="#">
+        <img class="ml-5 py-3" src="../assets/logo.svg" alt />
+      </a>
+
+      <a href="#">
+        <i class="fab fa-linkedin fa-white"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-youtube fa-white"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-instagram fa-white"></i>
+      </a>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+#header {
+  background: #693250;
+  color: white;
+}
+
+.fa-white {
+  color: white;
+  float: right;
+  padding: 2em 0.7em 0 0;
+}
+</style>
+
---
+++ Frontend/cv-bank/src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="container mt-5">
+      <h3 class="text-center">Välkommen till IT-Högskolans Cv-databas</h3>
+      <div class="form-container mx-auto">
+        <form>
+          <div class="form-group">
+            <label for>Användarnamn:</label>
+            <input type="email" class="form-control" aria-describedby="emailHelp" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Lösenord:</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" />
+          </div>
+          <button type="submit" class="btn-color">Logga in</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style scoped>
+.form-container {
+  width: 500px;
+  border: 2px solid #eee;
+  margin-top: 5em;
+}
+
+form {
+  padding: 2em;
+}
+</style>
